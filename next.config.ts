a41fef2bbc8c393632fe36a25c14500754de3937
Jiayich0 <jiayich.p@gmail.com>
2025-05-27 14:02:04 +0200

    final

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,13 @@ const nextConfig = {
        return [
            {
                source: '/api/pokemon',
-               destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+               destination: 'https://tsekpw0vd7.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+               //destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
            },
            {
                source: '/api/pokemon/:id',
-               destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+               destination: 'https://tsekpw0vd7.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+               //destination: 'https://rpr35yaoze.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
            },
        ];
    }
