75722eb593e91534b0ade4444e2eed72434002e3
Jiayich0 <jiayich.p@gmail.com>
2025-05-27 13:30:28 +0200

    Corregido con Stage

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,11 @@ const nextConfig = {
        return [
            {
                source: '/api/pokemon',
-               destination: 'https://tsekpw0vd7.execute-api.eu-west-2.amazonaws.com/Prod/pokemon',
+               destination: 'https://gw3k9yom6l.execute-api.eu-west-2.amazonaws.com/Stage/pokemon',
            },
            {
                source: '/api/pokemon/:id',
-               destination: 'https://tsekpw0vd7.execute-api.eu-west-2.amazonaws.com/Prod/pokemon/:id',
+               destination: 'https://gw3k9yom6l.execute-api.eu-west-2.amazonaws.com/Stage/pokemon/{id}',
            },
        ];
    }
